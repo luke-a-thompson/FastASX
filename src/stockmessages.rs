07c2790edc209048b5b0f2bc3df95318b62a87dd
leftwinglow <luke-a-thompson@outlook.com>
2024-08-22 01:06:20 +1000

    StockMessage tests

--- src/stockmessages.rs
+++ src/stockmessages.rs
@@ -1,13 +1,16 @@
 use crate::enums::{
     BoolOrUnavailable, FinancialStatusIndicator, IPOReleaseQualifier, IssueClassificationValues,
     MWCBLevel, MarketCategory, MarketMakerMode, MarketParticipantState, RegSHOAction,
-    ShortSaleThresholdIndicator, TradingHaltReasonCodes, TradingReasonCodes,
-    TradingResumptionReasonCodes, TradingState,
+    ShortSaleThresholdIndicator, TradingReasonCodes, TradingState,
 };
 use crate::helpers::{byte_to_bool, byte_to_bool_space, u8s_to_ticker};
 use crate::messageheader::MessageHeader;
-use crate::types::{Parse, ParseError, Stock};
+use crate::types::{BinaryMessageLength, EnumTestHelpers, Parse, ParseError, Stock};
 use byteorder::{BigEndian, ByteOrder};
+
+use crate::types::GenerateBinaryExample;
+#[cfg(test)]
+use fastrand::Rng;
 
 #[derive(Debug, PartialEq)]
 pub struct StockDirectory {
@@ -30,8 +33,10 @@ pub struct StockDirectory {
 
 impl Parse for StockDirectory {
     fn parse(input: &[u8]) -> Result<Self, ParseError> {
-        if input.len() != 38 {
-            panic!("Invalid input length for StockDirectory");
+        if input.len() != Self::LENGTH {
+            return Err(ParseError::IncompleteMessage {
+                expected: Self::LENGTH,
+            });
         }
 
         Ok(StockDirectory {
@@ -54,6 +59,53 @@ impl Parse for StockDirectory {
     }
 }
 
+impl BinaryMessageLength for StockDirectory {
+    const LENGTH: usize = 38;
+}
+
+#[cfg(test)]
+impl GenerateBinaryExample<{ Self::LENGTH }> for StockDirectory {
+    fn generate_example_message() -> [u8; Self::LENGTH] {
+        let mut rng = Rng::new();
+
+        let header = MessageHeader::generate_example_message();
+        let stock = rng.u64(..).to_be_bytes();
+        let market_category = MarketCategory::generate_example_code();
+        let financial_status_indicator = FinancialStatusIndicator::generate_example_code();
+        let round_lot_size = rng.u32(..).to_be_bytes();
+        let round_lots_only = b'Y';
+        let issue_classification = IssueClassificationValues::generate_example_code();
+        let issue_sub_type = rng.u16(..).to_be_bytes();
+        let authenticity = b'P';
+        let short_sale_threshold_indicator = ShortSaleThresholdIndicator::generate_example_code();
+        let ipo_flag = b'N';
+        let luld_reference_price_tier = b' ';
+        let etp_flag = b' ';
+        let etp_leverage_factor = rng.u32(..).to_be_bytes();
+        let inverse_indicator = b'N';
+
+        // Concatenate the arrays into a final message
+        let mut message = [0u8; Self::LENGTH];
+        message[..10].copy_from_slice(&header);
+        message[10..18].copy_from_slice(&stock);
+        message[18] = market_category;
+        message[19] = financial_status_indicator;
+        message[20..24].copy_from_slice(&round_lot_size);
+        message[24] = round_lots_only;
+        message[25] = issue_classification;
+        message[26..28].copy_from_slice(&issue_sub_type);
+        message[28] = authenticity as u8;
+        message[29] = short_sale_threshold_indicator;
+        message[30] = ipo_flag as u8;
+        message[31] = luld_reference_price_tier as u8;
+        message[32] = etp_flag as u8;
+        message[33..37].copy_from_slice(&etp_leverage_factor);
+        message[37] = inverse_indicator;
+
+        message
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct StockTradingAction {
     header: MessageHeader,
@@ -65,8 +117,10 @@ pub struct StockTradingAction {
 
 impl Parse for StockTradingAction {
     fn parse(input: &[u8]) -> Result<Self, ParseError> {
-        if input.len() != 24 {
-            return Err(ParseError::IncompleteMessage { expected: 24 });
+        if input.len() != Self::LENGTH {
+            return Err(ParseError::IncompleteMessage {
+                expected: Self::LENGTH,
+            });
         }
 
         Ok(StockTradingAction {
@@ -79,6 +133,33 @@ impl Parse for StockTradingAction {
     }
 }
 
+impl BinaryMessageLength for StockTradingAction {
+    const LENGTH: usize = 24;
+}
+
+#[cfg(test)]
+impl GenerateBinaryExample<{ Self::LENGTH }> for StockTradingAction {
+    fn generate_example_message() -> [u8; Self::LENGTH] {
+        let mut rng = Rng::new();
+
+        let header = MessageHeader::generate_example_message();
+        let stock = rng.u64(..).to_be_bytes();
+        let trading_state = TradingState::generate_example_code();
+        let reserved = rng.u8(..);
+        let reason = b"IPO1";
+
+        // Concatenate the arrays into a final message
+        let mut message = [0u8; Self::LENGTH];
+        message[..10].copy_from_slice(&header);
+        message[10..18].copy_from_slice(&stock);
+        message[18] = trading_state;
+        message[19] = reserved;
+        message[19..23].copy_from_slice(reason);
+
+        message
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct RegSHOShortSalePriceTestRestriction {
     header: MessageHeader,
@@ -88,8 +169,10 @@ pub struct RegSHOShortSalePriceTestRestriction {
 
 impl Parse for RegSHOShortSalePriceTestRestriction {
     fn parse(input: &[u8]) -> Result<Self, ParseError> {
-        if input.len() != 19 {
-            return Err(ParseError::IncompleteMessage { expected: 19 });
+        if input.len() != Self::LENGTH {
+            return Err(ParseError::IncompleteMessage {
+                expected: Self::LENGTH,
+            });
         }
 
         Ok(RegSHOShortSalePriceTestRestriction {
@@ -97,6 +180,29 @@ impl Parse for RegSHOShortSalePriceTestRestriction {
             stock: input[10..18].try_into().unwrap(), // We only read up to index 18, 1 less because of match. max spec offset-1
             reg_sho_action: RegSHOAction::try_from(input[18])?,
         })
+    }
+}
+
+impl BinaryMessageLength for RegSHOShortSalePriceTestRestriction {
+    const LENGTH: usize = 19;
+}
+
+#[cfg(test)]
+impl GenerateBinaryExample<{ Self::LENGTH }> for RegSHOShortSalePriceTestRestriction {
+    fn generate_example_message() -> [u8; Self::LENGTH] {
+        let mut rng = Rng::new();
+
+        let header = MessageHeader::generate_example_message();
+        let stock = rng.u64(..).to_be_bytes();
+        let reg_sho_action = RegSHOAction::generate_example_code();
+
+        // Concatenate the arrays into a final message
+        let mut message = [0u8; Self::LENGTH];
+        message[..10].copy_from_slice(&header);
+        message[10..18].copy_from_slice(&stock);
+        message[18] = reg_sho_action;
+
+        message
     }
 }
 
@@ -112,8 +218,10 @@ pub struct MarketParticipantPosition {
 
 impl Parse for MarketParticipantPosition {
     fn parse(input: &[u8]) -> Result<Self, ParseError> {
-        if input.len() != 25 {
-            return Err(ParseError::IncompleteMessage { expected: 25 });
+        if input.len() != Self::LENGTH {
+            return Err(ParseError::IncompleteMessage {
+                expected: Self::LENGTH,
+            });
         }
 
         Ok(MarketParticipantPosition {
@@ -124,6 +232,35 @@ impl Parse for MarketParticipantPosition {
             market_maker_mode: MarketMakerMode::try_from(input[23])?,
             market_participant_state: MarketParticipantState::try_from(input[24])?,
         })
+    }
+}
+
+impl BinaryMessageLength for MarketParticipantPosition {
+    const LENGTH: usize = 25;
+}
+
+#[cfg(test)]
+impl GenerateBinaryExample<{ Self::LENGTH }> for MarketParticipantPosition {
+    fn generate_example_message() -> [u8; Self::LENGTH] {
+        let mut rng = Rng::new();
+
+        let header = MessageHeader::generate_example_message();
+        let mp_id = rng.u32(..).to_be_bytes();
+        let stock = rng.u64(..).to_be_bytes();
+        let primary_market_maker = b'Y';
+        let market_maker_mode = MarketMakerMode::generate_example_code();
+        let market_participant_state = MarketParticipantState::generate_example_code();
+
+        // Concatenate the arrays into a final message
+        let mut message = [0u8; Self::LENGTH];
+        message[..10].copy_from_slice(&header);
+        message[10..14].copy_from_slice(&mp_id);
+        message[14..22].copy_from_slice(&stock);
+        message[22] = primary_market_maker;
+        message[23] = market_maker_mode;
+        message[24] = market_participant_state;
+
+        message
     }
 }
 
@@ -138,8 +275,10 @@ pub struct MWCBDeclineLevel {
 
 impl Parse for MWCBDeclineLevel {
     fn parse(input: &[u8]) -> Result<Self, ParseError> {
-        if input.len() != 35 {
-            return Err(ParseError::IncompleteMessage { expected: 35 });
+        if input.len() != Self::LENGTH {
+            return Err(ParseError::IncompleteMessage {
+                expected: Self::LENGTH,
+            });
         }
 
         Ok(MWCBDeclineLevel {
@@ -151,6 +290,31 @@ impl Parse for MWCBDeclineLevel {
     }
 }
 
+impl BinaryMessageLength for MWCBDeclineLevel {
+    const LENGTH: usize = 35;
+}
+
+#[cfg(test)]
+impl GenerateBinaryExample<{ Self::LENGTH }> for MWCBDeclineLevel {
+    fn generate_example_message() -> [u8; Self::LENGTH] {
+        let mut rng = Rng::new();
+
+        let header = MessageHeader::generate_example_message();
+        let level1 = rng.u64(..).to_be_bytes();
+        let level2 = rng.u64(..).to_be_bytes();
+        let level3 = rng.u64(..).to_be_bytes();
+
+        // Concatenate the arrays into a final message
+        let mut message = [0u8; Self::LENGTH];
+        message[..10].copy_from_slice(&header);
+        message[10..18].copy_from_slice(&level1);
+        message[18..26].copy_from_slice(&level2);
+        message[26..34].copy_from_slice(&level3);
+
+        message
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct MWCBStatus {
     header: MessageHeader,
@@ -159,14 +323,35 @@ pub struct MWCBStatus {
 
 impl Parse for MWCBStatus {
     fn parse(input: &[u8]) -> Result<Self, ParseError> {
-        if input.len() != 11 {
-            return Err(ParseError::IncompleteMessage { expected: 11 });
+        if input.len() != Self::LENGTH {
+            return Err(ParseError::IncompleteMessage {
+                expected: Self::LENGTH,
+            });
         }
 
         Ok(MWCBStatus {
             header: MessageHeader::parse(&input[..10]),
             breached_level: MWCBLevel::try_from(input[10])?,
         })
+    }
+}
+
+impl BinaryMessageLength for MWCBStatus {
+    const LENGTH: usize = 11;
+}
+
+#[cfg(test)]
+impl GenerateBinaryExample<{ Self::LENGTH }> for MWCBStatus {
+    fn generate_example_message() -> [u8; Self::LENGTH] {
+        let header = MessageHeader::generate_example_message();
+        let breached_level = MWCBLevel::generate_example_code();
+
+        // Concatenate the arrays into a final message
+        let mut message = [0u8; Self::LENGTH];
+        message[..10].copy_from_slice(&header);
+        message[10] = breached_level;
+
+        message
     }
 }
 
@@ -181,8 +366,10 @@ pub struct IPOQuotingPeriodUpdate {
 
 impl Parse for IPOQuotingPeriodUpdate {
     fn parse(input: &[u8]) -> Result<Self, ParseError> {
-        if input.len() != 27 {
-            return Err(ParseError::IncompleteMessage { expected: 27 });
+        if input.len() != Self::LENGTH {
+            return Err(ParseError::IncompleteMessage {
+                expected: Self::LENGTH,
+            });
         }
 
         Ok(IPOQuotingPeriodUpdate {
@@ -192,5 +379,32 @@ impl Parse for IPOQuotingPeriodUpdate {
             ipo_quotation_release_qualifier: IPOReleaseQualifier::try_from(input[22])?,
             ipo_price: BigEndian::read_u32(&input[23..27]),
         })
+    }
+}
+
+impl BinaryMessageLength for IPOQuotingPeriodUpdate {
+    const LENGTH: usize = 27;
+}
+
+#[cfg(test)]
+impl GenerateBinaryExample<{ Self::LENGTH }> for IPOQuotingPeriodUpdate {
+    fn generate_example_message() -> [u8; Self::LENGTH] {
+        let mut rng = Rng::new();
+
+        let header = MessageHeader::generate_example_message();
+        let stock = rng.u64(..).to_be_bytes();
+        let ipo_quotation_release_time = rng.u32(..).to_be_bytes();
+        let ipo_quotation_release_qualifier = IPOReleaseQualifier::generate_example_code();
+        let ipo_price = rng.u32(..).to_be_bytes();
+
+        // Concatenate the arrays into a final message
+        let mut message = [0u8; Self::LENGTH];
+        message[..10].copy_from_slice(&header);
+        message[10..18].copy_from_slice(&stock);
+        message[18..22].copy_from_slice(&ipo_quotation_release_time);
+        message[22] = ipo_quotation_release_qualifier;
+        message[23..27].copy_from_slice(&ipo_price);
+
+        message
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,6 @@
+use stockmessages::{
+    IPOQuotingPeriodUpdate, MWCBDeclineLevel, MWCBStatus, MarketParticipantPosition, RegSHOShortSalePriceTestRestriction, StockDirectory, StockTradingAction
+};
 use types::GenerateBinaryExample;
 
 use super::*;
@@ -7,4 +10,68 @@ fn test_system_event_message() {
     let example_msg = SystemEventMessage::generate_example_message();
     let parsed = SystemEventMessage::parse(&example_msg);
     assert!(parsed.is_ok(), "Parsing the system event message failed");
+}
+
+#[test]
+fn test_stock_directory() {
+    let example_msg = StockDirectory::generate_example_message();
+    let parsed = StockDirectory::parse(&example_msg);
+    assert!(parsed.is_ok(), "Parsing the stock directory message failed");
+}
+
+#[test]
+fn test_stock_trading_action() {
+    let example_msg = StockTradingAction::generate_example_message();
+    let parsed = StockTradingAction::parse(&example_msg);
+    assert!(
+        parsed.is_ok(),
+        "Parsing the stock trading action message failed"
+    );
+}
+
+#[test]
+fn test_reg_sho_short_sale_price_test_restriction() {
+    let example_msg = RegSHOShortSalePriceTestRestriction::generate_example_message();
+    let parsed = RegSHOShortSalePriceTestRestriction::parse(&example_msg);
+    assert!(
+        parsed.is_ok(),
+        "Parsing the reg sho short sale price test restriction message failed"
+    );
+}
+
+#[test]
+fn test_market_participant_position() {
+    let example_msg = MarketParticipantPosition::generate_example_message();
+    let parsed = MarketParticipantPosition::parse(&example_msg);
+    assert!(
+        parsed.is_ok(),
+        "Parsing the market participant position message failed"
+    );
+}
+
+#[test]
+fn test_mwcb_decline_level() {
+    let example_msg = MWCBDeclineLevel::generate_example_message();
+    let parsed = MWCBDeclineLevel::parse(&example_msg);
+    assert!(
+        parsed.is_ok(),
+        "Parsing the mwcb decline level message failed"
+    );
+}
+
+#[test]
+fn test_mwcb_status() {
+    let example_msg = MWCBStatus::generate_example_message();
+    let parsed = MWCBStatus::parse(&example_msg);
+    assert!(parsed.is_ok(), "Parsing the mwcb status message failed");
+}
+
+#[test]
+fn test_ipo_quoting_period_update() {
+    let example_msg = IPOQuotingPeriodUpdate::generate_example_message();
+    let parsed = IPOQuotingPeriodUpdate::parse(&example_msg);
+    assert!(
+        parsed.is_ok(),
+        "Parsing the ipo quoting period update message failed"
+    );
 }
